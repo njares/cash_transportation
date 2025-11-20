9c9a75e3c32b07edd9e49cddb95c7ae5335aea4b
Nicolas Jares <njares@durga.famaf.unc.edu.ar>
2025-11-20 16:57:31 -0300

    Experimentos con std variable

---
+++ scripts/job_25_11_17.sh
@@ -0,0 +1,5 @@
+#python3 cash_transportation/scripts/experimento_2.py --threads 56 --n-min 4 --n-max 150 --solver gurobi --exp-id exp_std_0.json --C-std .0 --V-std .0
+python3 cash_transportation/scripts/experimento_2.py --threads 56 --n-min 4 --n-max 150 --solver gurobi --exp-id exp_std_25.json --C-std .25 --V-std .25
+python3 cash_transportation/scripts/experimento_2.py --threads 56 --n-min 4 --n-max 150 --solver gurobi --exp-id exp_std_50.json --C-std .50 --V-std .50
+python3 cash_transportation/scripts/experimento_2.py --threads 56 --n-min 4 --n-max 150 --solver gurobi --exp-id exp_std_75.json --C-std .75 --V-std .75
+python3 cash_transportation/scripts/experimento_2.py --threads 56 --n-min 4 --n-max 150 --solver gurobi --exp-id exp_std_100.json --C-std 1.0 --V-std 1.0
