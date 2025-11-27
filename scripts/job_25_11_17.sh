9031ea215b65ff238a2a7f5d3d9dc7bce9cdf758
Nicolás Jares <nico.jar@gmail.com>
2025-11-27 14:43:24 -0300

    experimento con std 0

--- scripts/job_25_11_17.sh
+++ scripts/job_25_11_17.sh
@@ -1,4 +1,4 @@
-#python3 cash_transportation/scripts/experimento_2.py --threads 56 --n-min 4 --n-max 150 --solver gurobi --exp-id exp_std_0.json --C-std .0 --V-std .0
+python3 cash_transportation/scripts/experimento_2.py --threads 56 --n-min 4 --n-max 150 --solver gurobi --exp-id exp_std_0.json --C-std .0 --V-std .0
 python3 cash_transportation/scripts/experimento_2.py --threads 56 --n-min 4 --n-max 150 --solver gurobi --exp-id exp_std_25.json --C-std .25 --V-std .25
 python3 cash_transportation/scripts/experimento_2.py --threads 56 --n-min 4 --n-max 150 --solver gurobi --exp-id exp_std_50.json --C-std .50 --V-std .50
 python3 cash_transportation/scripts/experimento_2.py --threads 56 --n-min 4 --n-max 150 --solver gurobi --exp-id exp_std_75.json --C-std .75 --V-std .75
