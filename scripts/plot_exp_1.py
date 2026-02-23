6648e7d391e9b859412dcd6e60caf6ca8595ab39
Nicolás Jares <nico.jar@gmail.com>
2026-02-23 16:25:10 -0300

    estilo de graficos para articulo

--- scripts/plot_exp_1.py
+++ scripts/plot_exp_1.py
@@ -8,6 +8,7 @@ import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import matplotlib as mpl
 
 
 def plot_ganancias(tabla_df, output_file=None):
@@ -30,27 +31,41 @@ def plot_ganancias(tabla_df, output_file=None):
     value_cols = ['mean_0', 'mean_1', 'mean_2', 'mean_3', 'mean_4']
     error_cols = ['std_0', 'std_1', 'std_2', 'std_3', 'std_4']
     labels = ['Capacity 1', 'Capacity 3/4', 'Capacity 1/2', 'Capacity 1/3', 'Capacity 1/4']
-
+    fmt_list = ['-o', '-s', '-^', '-d', '-p']
+    
+    # Configuración matplotlib
+    mpl.rcParams['pdf.fonttype'] = 42
+    mpl.rcParams['ps.fonttype'] = 42
+    plt.rcParams['text.usetex'] = True
+    mpl.rcParams['font.family'] = 'Computer Modern'
+    
+    #fig = plt.figure(figsize=(2.85,2.85),dpi=300) # Tamaño en pulgadas (ancho, alto)
+    fig = plt.figure(figsize=(6,6),dpi=300) # Tamaño en pulgadas (ancho, alto)
+    ax = plt.axes((0.1,0.1,0.8,0.8)) # Tamaño en (0,1), (left, bottom, width, height)
+    
+    ax.set_xticks([1,2,3,4,5,6,7,8,9,10])
+    ax.set_yticks([0,10,20,30,40,50,60,70])
+    
     for i, col in enumerate(value_cols):
-        plt.errorbar(
+        ax.errorbar(
             plot_df['interes'],
             plot_df[col],
             yerr=plot_df[error_cols[i]],
-            fmt='-o', # Format: line with circles
+            fmt=fmt_list[i], # Format: line with circles
             label=labels[i], # Label for legend
             capsize=4 # Size of the error bar caps
         )
     
-    plt.xlabel('I')
-    plt.ylabel('Percentage')
-    plt.title(f'Gain')
-    plt.legend()
-    plt.grid(True)
-    plt.ylim(-5, 75)
+    ax.set_xlabel('Interest ($\mu$)')
+    ax.set_ylabel('Mean Gain')
+    # plt.title(f'Gain')
+    ax.legend()
+    # plt.grid(True)
+    ax.set_ylim(-5, 75)
     
     # Guardar
     plt.savefig(output_file, dpi=300, bbox_inches='tight')
-    plt.clf()
+    plt.close(fig)
     print(f"Gráfico guardado en: {output_file}")
 
 
